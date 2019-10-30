7753c2d58ef10ed1bd38d9ff236ee041d23bc91a
Josh Merrill <joshmmerrill@outlook.com>
2019-10-29 20:41:30 -0700

    Finished first question.

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+var userName = prompt ('What is your name?');
+alert('Hello, ' + userName + '.');
+console.log(userName);
+
+var userKnow = prompt ('Do you know me?');
+userKnow = userKnow.toLowerCase();
+
+if (userKnow === 'yes' || userKnow === 'y') {
+  console.log('Yes');
+} else {
+  alert ('Uh, yeah you do.');
+  console.log('No');
+}
