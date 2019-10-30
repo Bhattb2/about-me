587b9900e7ea89d5ac86f70629efd199a438dd0e
Josh Merrill <joshmmerill@outlook.com>
2019-10-29 21:02:28 -0700

    Added second and third prompts to page.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 'use strict';
-var userName = prompt ('What is your name?');
-alert('Hello, ' + userName + '.');
-console.log(userName);
 
 var userKnow = prompt ('Do you know me?');
 userKnow = userKnow.toLowerCase();
 
 if (userKnow === 'yes' || userKnow === 'y') {
   console.log('Yes');
+  alert ('What up! I know you, too.')
 } else {
   alert ('Uh, yeah you do.');
   console.log('No');
 }
-console.log('userName');
+
+var userCode = prompt ('Am I taking a CodeFellows course?');
+userCode = userCode.toLowerCase();
+
+if (userCode === 'yes' || userCode === 'y') {
+  console.log('Yes');
+  alert ('Damn right I am.')
+} else {
+  alert ('Uh, yeah I am.');
+  console.log('Do you even me?');
+}
+
+var userName = prompt ('What is your name?');
+alert('Hello, ' + userName + '.');
+console.log(userName);
