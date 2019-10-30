cdb14868389dee0c6ddc412c5470b2e02cde854a
Josh Merrill <joshmmerill@outlook.com>
2019-10-29 21:20:23 -0700

    Added the remainder of prompt questions.

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ var userKnow = prompt ('Do you know me?');
 userKnow = userKnow.toLowerCase();
 
 if (userKnow === 'yes' || userKnow === 'y') {
+  alert ('What up! I know you, too.');
   console.log('Yes');
-  alert ('What up! I know you, too.')
 } else {
   alert ('Uh, yeah you do.');
   console.log('No');
@@ -15,10 +15,43 @@ var userCode = prompt ('Am I taking a CodeFellows course?');
 userCode = userCode.toLowerCase();
 
 if (userCode === 'yes' || userCode === 'y') {
+  alert ('Damn right I am.');
   console.log('Yes');
-  alert ('Damn right I am.')
 } else {
   alert ('Uh, yeah I am.');
+  console.log('Do you even me?');
+}
+
+var userCollege = prompt ('Did I attend college?');
+userCollege = userCollege.toLowerCase();
+
+if (userCollege === 'yes' || userCollege === 'y') {
+  alert ('I most certainly did. I Northern Arizona University. Go Lumberjacks!');
+  console.log('Yes');
+} else {
+  alert ('Hey now, I am smart, okay!');
+  console.log('Do you even me?');
+}
+
+var userCity = prompt ('Do I live in Seattle?');
+userCity = userCity.toLowerCase();
+
+if (userCity === 'yes' || userCity === 'y') {
+  alert ('Yup! I live in Fremont.');
+  console.log('Yes');
+} else {
+  alert ('What, you think I live in the burbs? Aw hell no!');
+  console.log('Do you even me?');
+}
+
+var userJob = prompt ('Am I employed?');
+userCity = userCity.toLowerCase();
+
+if (userJob === 'yes' || userJob=== 'y') {
+  alert ('Yup! I work for Alaska Airlines.');
+  console.log('Yes');
+} else {
+  alert ('Wrong! I am employed by Alaska Airlines.');
   console.log('Do you even me?');
 }
 
