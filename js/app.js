a83305f4f30bfed82368a5c0a79127c399d7f124
Josh Merrill <joshmmerill@outlook.com>
2019-10-31 21:03:44 -0700

    Added qustion #7 to JS, updated console logs an added list updates to HTML and defined ol ul and li styles in CSS

--- js/app.js
+++ js/app.js
@@ -1,60 +1,92 @@
 'use strict';
 
+// Question #1: Do you know me?
 var userKnow = prompt ('Do you know me?');
 userKnow = userKnow.toLowerCase();
 
+console.log ('Do you know me?:', userKnow); // added correct console.log style
+
 if (userKnow === 'yes' || userKnow === 'y') {
   alert ('What up! I know you, too.');
-  // console.log('Yes');
 } else {
   alert ('Uh, yeah you do.');
-  // console.log('No');
 }
 
+// Question #2: Am I taking a CodeFellows course?
 var userCode = prompt ('Am I taking a CodeFellows course?');
 userCode = userCode.toLowerCase();
 
+console.log ('Am I taking a CodeFellows course?:', userCode); // added correct console.log style
+
 if (userCode === 'yes' || userCode === 'y') {
   alert ('Damn right I am.');
-  // console.log('Yes');
 } else {
   alert ('Uh, yeah I am.');
-  // console.log('Do you even me?');
 }
 
+// Question #3: Did I attend college?
 var userCollege = prompt ('Did I attend college?');
 userCollege = userCollege.toLowerCase();
 
+console.log ('Did I attend college?:', userCollege); // added correct console.log style
+
 if (userCollege === 'yes' || userCollege === 'y') {
   alert ('I most certainly did. I Northern Arizona University. Go Lumberjacks!');
-  // console.log('Yes');
 } else {
   alert ('Hey now, I am smart, okay!');
-  // console.log('Do you even me?');
 }
 
+// Question #4: Do I live in Seattle?
 var userCity = prompt ('Do I live in Seattle?');
 userCity = userCity.toLowerCase();
 
+console.log ('Do I live in Seattle?:', userCity); // added correct console.log style
+
 if (userCity === 'yes' || userCity === 'y') {
   alert ('Yup! I live in Fremont.');
-  // console.log('Yes');
 } else {
   alert ('What, you think I live in the burbs? Aw hell no!');
-  // console.log('Do you even me?');
 }
 
+// Question #5: Am I employed?
 var userJob = prompt ('Am I employed?');
 userJob = userJob.toLowerCase();
 
+console.log ('Am I employed?:', userJob); // added correct console.log style
+
 if (userJob === 'yes' || userJob=== 'y') {
   alert ('Yup! I work for Alaska Airlines.');
-  // console.log('Yes');
 } else {
   alert ('Wrong! I am employed by Alaska Airlines.');
-  // console.log('Do you even me?');
 }
 
+// Question #6: What is your name?
 var userName = prompt ('What is your name?');
+
+console.log ('What is your name?:', userName); // added correct console.log style
+
 alert('Hello, ' + userName + '.');
-// console.log(userName);
+
+// sixth question
+
+var userAge = prompt ('How old am I?');
+userAge = parseFloat(userAge);
+console.log (typeof userAge);
+if (userAge > (30)) {
+  alert ('Too high!');
+} else if (userAge === 30) {
+  alert ('You guessed it right!');
+} else if (userAge < 30) {
+  alert ('Too low!');
+}
+
+for (var i = 0; i < 4; i++) {
+  document.write(i);
+}
+
+// var i = 0;
+
+// while(i < 4) {
+//   console.log(i)
+//   i = i + 1;
+// }
